dee6a304d0526bcec902605a6f8154da3448bc1d
atomfenrir <adam.courtney@karzulia.com>
2014-06-09 15:41:43 -0400

    Create restarter.sh

---
+++ restarter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# The intention of this script is to initiate a loop that will check for Starbound's server status. 
+# If Starbound is found to be not online, then it will start Starbound.
+#
+
+
