43d3ad735002774d4cd593b45def3d0a6dc1c0a5
atomfenrir <adam.courtney@karzulia.com>
2014-06-10 06:00:07 -0400

    Update restarter.sh
    Work in progress

--- restarter.sh
+++ restarter.sh
@@ -4,4 +4,21 @@
 # If Starbound is found to be not online, then it will start Starbound.
 #
 
+$starbound_dir="/path/to/starbound/linux64or32" # specify your starbound server folder here, starting from /home/username, etc, if necessary
+$starbound_file="starbound_server" # may be called something else in your install
 
+while true
+do
+
+$starb_status= #insert way to determine starbound status here xD
+
+$starb_status==1 then 
+
+sleep 15
+done
+
+
+starter () {
+  cd $starbound_dir
+  bash $starbound_file
+}
