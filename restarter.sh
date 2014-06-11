5e950528d8ff41b26bf2fd8cdd4d95df57714ffd
atomfenrir <adam.courtney@karzulia.com>
2014-06-11 17:29:13 -0400

    Update restarter.sh

--- restarter.sh
+++ restarter.sh
@@ -12,14 +12,22 @@ starbound_dir="/path/to/starbound/linux64or32"
 #ie if a match can be obtained on more than one running process using this name, it will not recognize when the main server goes down.
 starbound_file="starbound_server"
 
+# initializes the background screen to run on
+screen -dmS starbound
 
 #the function that will do the leg work when Starbound is found to not be running....
-starter () {
+restarter () {
   echo "starbound-restarter is restarting Starbound...."
+  screen -x starbound
   cd $starbound_dir
-  bash $starbound_file
+  ./$starbound_file
+  screen -d
+  sleep 15 # because it takes a minute for starbound_server to get off the ground
   return
 }
+
+# saving this timestamp for later
+# timestamp=`date +%Y_%m%d_%H%M`
 
 #the core loop of the restarter that checks for downages every 30 secs...
 while true
@@ -28,7 +36,7 @@ do
 ps -ag|grep -v "grep"|grep $starbound_file
 
 if [ $? -eq 1 ]
-then starter
+then restarter
 else echo "Starbound server looks okay.... "
 fi
 
